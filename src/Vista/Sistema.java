15c4286e5fe3794f18e58652ce5d8bdaa6808b23
eliseo <eliseogalvanmora1@gmail.com>
2023-09-20 13:20:58 -0600

    Registro de clientes en base de datos

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -5,14 +5,18 @@
  */
 package Vista;
 
+import Modelo.Cliente;
+import Modelo.ClienteDAO;
 import Modelo.login;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Eliseo Galván
  */
 public class Sistema extends javax.swing.JFrame {
-
+Cliente cl = new Cliente();
+ClienteDAO client = new ClienteDAO();
     /**
      * Creates new form Sistema
      */
@@ -422,6 +426,11 @@ public class Sistema extends javax.swing.JFrame {
 
         btnGuardarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/GuardarTodo.png"))); // NOI18N
         btnGuardarCliente.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnGuardarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarClienteActionPerformed(evt);
+            }
+        });
 
         btnEditarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/Actualizar (2).png"))); // NOI18N
         btnEditarCliente.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -791,7 +800,6 @@ public class Sistema extends javax.swing.JFrame {
         }
 
         btnPdfVentas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/pdf.png"))); // NOI18N
-        btnPdfVentas.setActionCommand("");
 
         javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
         jPanel6.setLayout(jPanel6Layout);
@@ -938,6 +946,20 @@ public class Sistema extends javax.swing.JFrame {
     private void jTextField26ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField26ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField26ActionPerformed
+
+    private void btnGuardarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarClienteActionPerformed
+                               if(!"".equals(txtDniCliente.getText()) || !"".equals(txtNombreCliente.getText()) || !"".equals(txtTelefonoCliente.getText()) || !"".equals(txtDireccionCliente.getText())){
+                                   cl.setDni(Integer.parseInt(txtDniCliente.getText()));
+                                   cl.setNombre(txtNombreCliente.getText());
+                                   cl.setTelefono(Integer.parseInt(txtTelefonoCliente.getText()));
+                                   cl.setDireccion((txtDireccionCliente.getText()));
+                                   cl.setRazon(txtRazonCliente.getText());
+                                   client.RegistrarCliente(cl);
+                                   JOptionPane.showMessageDialog(null,"Cliente Registrado");
+                                    } else{
+                                   JOptionPane.showMessageDialog(null,"Los campos estan vacios");
+                               }
+    }//GEN-LAST:event_btnGuardarClienteActionPerformed
 
     /**
      * @param args the command line arguments
